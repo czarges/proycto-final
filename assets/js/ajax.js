b2f7ae70ed04e0f43e5ee427fa2ba43eb51ba3a0
deltaoscargolf <abrdog2@gmail.com>
2018-12-03 22:10:57 -0300

    commit

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,22 +1,88 @@
 $.ajax({
-	url: 'https://randomuser.me/api/?results=50',
+	url: 'https://randomuser.me/api/?results=12',
 	type: 'GET',
 	dataType: 'json',
 })
 .done(function(data) {
-	console.log(data);
+	console.log(data.results);
 
-	var user = $ ('user')
-	var profile = data.results[0]
+	$.each( data.results, function( key, value ) {
+		$('.user').append(`<li class="col-md-4">
+	<img src="${value.picture.large}" class="user_picture" alt="">
+	<h2 class="user_name">${value.name.first} ${value.name.last}</h2>
 
-	user.children('.user_picture').attr('src',profile.picture.large)
-	user.children ('.user_name').text(profile.name.first + ' ' + profile.name.last)
-	user.children ('.user_email').attr ('href', 'mailto:' + profile.email).text(profile.email)
-	user.children ('.user_phone').text(profile.phone)
+</li>`)
+	});
 })
 .fail(function() {
 	console.log("error");
-})
-.always(function(data) {
-	console.log("data");
 });
+
+var coords = {
+ 	valdivia: {
+ 		lat: -39.8142200,
+ 		lng: -73.2458900
+ 	},
+ 	frutillar: {
+ 		lat: -41.1267600,
+ 		lng: -73.0437200
+ 	},
+ 	santiago: {
+ 		lat: -33.4488897,
+ 		lng: -70.6692655
+ 	}
+ };
+
+var image = {
+    'clear-day':'https://icons.wxug.com/i/c/v4/clear.svg',
+    'clear-night':'https://icons.wxug.com/i/c/v4/nt_clear.svg',
+    'partly-cloudy-day':'https://icons.wxug.com/i/c/v4/partlycloudy.svg',
+    'partly-cloudy-night':'https://icons.wxug.com/i/c/v4/nt_hazy.svg',
+    'cloudy':'https://icons.wxug.com/i/c/v4/cloudy.svg',
+    'rain':'https://icons.wxug.com/i/c/v4/rain.svg'
+  }
+
+var map = null;
+var marker = null;
+
+function initMap() {
+	var location = coords.santiago
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 13,
+		center: location
+	});
+
+	marker = new google.maps.Marker({
+		position: location,
+		map: map
+	});
+}
+
+var proxy = 'https://cors-anywhere.herokuapp.com/'
+var url = 'https://api.darksky.net/forecast/';
+var apiKey = 'e193913a8a2bab291b8999fae03fc8b7';
+var queryParams = ['exclude=[minutely,flags]', 'lang=es', 'units=auto'];
+
+$('#select').on('change', function() {
+	map.setCenter(coords[$(this).val()]);
+	marker.setMap(null);
+	marker = new google.maps.Marker({
+		position: (coords[$(this).val()]),
+		map: map
+	});
+
+	$.ajax({
+		url: proxy + url + apiKey + '/' + coords[$(this).val()].lat + ',' + coords[$(this).val()].lng + '?' + queryParams[0] + '&' + queryParams[1] + '&' + queryParams[2],
+		method: 'GET',
+		xhrFields: {cors: false}
+	}).then(function(data) {
+		console.log(data);
+		$('#resumen').text(parseInt(data.currently.temperature) + '° ' + data.currently.summary);
+		$('.img-responsive').attr('src',image[data.currently.icon]);
+	});
+
+});
+
+
+
+
